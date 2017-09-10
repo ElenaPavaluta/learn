7524813a21b4705721aa428fcbed4642c685281f
Ciprian Tanase <ciprian.dorin.tanase@ibm.com>
2017-09-10 22:19:53 +0300

    instance generic methods

--- learn.java/src/main/java/oc/p/chapters/c3_GenericsAndCollections/workingWithGenerics/genericInterfaces/interfaceMethods/instance/ShipC.java
+++ learn.java/src/main/java/oc/p/chapters/c3_GenericsAndCollections/workingWithGenerics/genericInterfaces/interfaceMethods/instance/ShipC.java
@@ -1,48 +1,25 @@
 package oc.p.chapters.c3_GenericsAndCollections.workingWithGenerics.genericInterfaces.interfaceMethods.instance;
 
-import java.time.LocalDate;
+public class ShipC<X, Y> implements Ship<X, Y>
+{
 
-public class ShipC<X, Y> implements Ship<X, Y> {
+	@Override public X m(Y y)
+	{
+		return null;
+	}
 
-    private static X x;
-    private static Y y;
+	@Override public <T> T m2(Y y)
+	{
+		return null;
+	}
 
-    private static ShipC<X, Y> inst;
+	@Override public <V> V m3(X x, Y y)
+	{
+		return null;
+	}
 
-    static {
-        inst = new ShipC<> ( );
-    }
-
-    static void m() {
-        x = inst.m ( y );
-        x = inst.<X>m ( y );
-        x = inst.<LocalDate>m ( y );  //because in the methods definition there's no definition for <> type the compiler just doesn't care
-    }
-
-    static void m2(){
-        x = inst.m2 ( y );
-        x = inst.<X> m2 ( y );
-//        x = inst.<LocalDate> m2 ( y );  //compiler error: X type is expected in <>
-
-    }
-
-    @Override
-    public X m(Y y) {
-        return null;
-    }
-
-    @Override
-    public <T> T m2(Y y) {
-        return null;
-    }
-
-    @Override
-    public <V> V m3(X x, Y y) {
-        return null;
-    }
-
-    @Override
-    public <T, Q> T m4(Y y, Q q) {
-        return null;
-    }
+	@Override public <T, Q> T m4(Y y, Q q)
+	{
+		return null;
+	}
 }
