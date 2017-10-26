af60b8a996ca9908a275dbe7c7fefe190181f2e5
Ciprian Tanase <ciprian.dorin.tanase@ibm.com>
2017-10-27 01:34:56 +0300

    27.10.2017

---
+++ learn.java/src/main/java/oc/p/chapters/c1_AdvancedClassDesign/workingWithEnums/addingContructorFieldsMethods/Run.java
@@ -0,0 +1,9 @@
+package oc.p.chapters.c1_AdvancedClassDesign.workingWithEnums.addingContructorFieldsMethods;
+
+class Run
+{
+	void m(){
+//		E e = new E(false);  //compiler error: an enum can not be instantiated or extended
+
+	}
+}
