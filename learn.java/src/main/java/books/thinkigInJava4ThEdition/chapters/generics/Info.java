a303c7a3eaf487fb7463b3ee725eb5d114bbe7f8
Ciprian Tanase <ciprian.dorin.tanase@ibm.com>
2018-04-30 16:22:32 +0300

    29.04.2018

--- learn.java/src/main/java/books/thinkigInJava4ThEdition/chapters/generics/Info.java
+++ learn.java/src/main/java/books/thinkigInJava4ThEdition/chapters/generics/Info.java
@@ -1,5 +1,6 @@
 package books.thinkigInJava4ThEdition.chapters.generics;
 
+
 /**
  * Because erasure removes type information in the body of a method,
  * what matters at run time is the boundaries:
@@ -8,5 +9,27 @@ package books.thinkigInJava4ThEdition.chapters.generics;
  * and inserts casting code.
  *
  * Erasure loses the ability to perform certain operations in generic code.
- * Anything that requires the knowledge of the exact type at run time won’t work.
+ *
+ * ANYTHING THAT REQUIRES THE KNOWLEDGE OF THE EXACT TYPE AT
+ * RUN TIME WON’T WORK.
+ *
+ * Because of erasure, the runtime type of an array (of generic) can only be Object[]
  */
+
+
+class Info<T>{
+    void m(){
+        /**
+         * Arrays keep track of their actual type, and that type is
+         * established at the point of creation of the array.
+         *
+         * THE ONLY WAY TO CREATE AN ARRAY OF GENERIC TYPE
+         */
+       T[] tArr = (T[])new Object[10];
+
+      T t = null;
+
+       tArr[0] =t;
+//       tArr[1] = new Object();  //compiler error
+    }
+}
---
+++ learn.java/src/main/java/books/thinkigInJava4ThEdition/chapters/generics/compensatingForErasure/creatingInstancesOfTypes/ex/_22/E.java
@@ -0,0 +1,4 @@
+package books.thinkigInJava4ThEdition.chapters.generics.compensatingForErasure.creatingInstancesOfTypes.ex._22;
+
+class E {
+}
