691a5df34e34c728b96f86ab554898c9279c0b5b
Ciprian Tanase <ciprian.dorin.tanase@ibm.com>
2018-05-02 18:08:42 +0300

    01.05.2018

---
+++ learn.java/src/main/java/books/thinkigInJava4ThEdition/chapters/generics/wildcards/SuperTypeWildcards.java
@@ -0,0 +1,14 @@
+package books.thinkigInJava4ThEdition.chapters.generics.wildcards;
+
+import java.util.List;
+
+/**
+ * FORBIDDEN: <T super MyClass>).
+ */
+class SuperTypeWildcards {
+    static void writeTo(List<? super Apple> apples){
+        apples.add(new Apple());
+        apples.add(new Jonathan());
+//        apples.add(new Fruit());  //compiler error
+    }
+}
---
+++ learn.java/src/main/java/books/thinkigInJava4ThEdition/chapters/generics/wildcards/tw/etendsVsSuper/Fruits.java
@@ -0,0 +1,25 @@
+package books.thinkigInJava4ThEdition.chapters.generics.wildcards.tw.etendsVsSuper;
+
+class Fruit {
+
+}
+
+class Apple extends Fruit {
+
+}
+
+class GoldApple extends Apple {
+
+}
+
+class SilverApple extends Apple {
+
+}
+
+class Citrix extends Fruit {
+
+}
+
+class Orange extends Citrix {
+
+}
