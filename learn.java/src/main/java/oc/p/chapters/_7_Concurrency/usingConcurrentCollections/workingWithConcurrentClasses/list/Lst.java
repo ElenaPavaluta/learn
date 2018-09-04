96f6b7a94568aa7d678bbfa5bda9c9558df910ee
Ciprian Tanase <ciprian.dorin.tanase@ibm.com>
2018-09-04 08:34:12 +0300

    03.09.2018

--- learn.java/src/main/java/oc/p/chapters/_7_Concurrency/usingConcurrentCollections/workingWithConcurrentClasses/list/Lst.java
+++ learn.java/src/main/java/oc/p/chapters/_7_Concurrency/usingConcurrentCollections/workingWithConcurrentClasses/list/Lst.java
@@ -1,5 +1,6 @@
 package oc.p.chapters._7_Concurrency.usingConcurrentCollections.workingWithConcurrentClasses.list;
 
+import java.util.Arrays;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 class Lst {
@@ -8,4 +9,23 @@ class Lst {
      * ->List
      */
     CopyOnWriteArrayList<Integer> copyOnWriteArrayList;
+
+    void m(){
+        copyOnWriteArrayList = new CopyOnWriteArrayList<>(Arrays.asList(1, 2, 3));
+
+        for(int i: copyOnWriteArrayList){
+            System.out.print(i + " ");
+            copyOnWriteArrayList.add(9);
+        }
+        System.out.println();
+        System.out.println("size: " + copyOnWriteArrayList.size());
+        System.out.println(copyOnWriteArrayList);
+    }
+
+    public static void main(String[] args) {
+        Lst l = new Lst();
+        l.m();
+    }
+
+
 }
--- learn.java/src/main/java/oc/p/chapters/_7_Concurrency/usingConcurrentCollections/workingWithConcurrentClasses/set/CopyOnWriteArraySet_.java
+++ learn.java/src/main/java/oc/p/chapters/_7_Concurrency/usingConcurrentCollections/workingWithConcurrentClasses/set/CopyOnWriteArraySet_.java
@@ -13,24 +13,23 @@ class CopyOnWriteArraySet_ {
 
     void m() throws InterruptedException {
 
-
         IntStream.range(0, 10)
                 .forEach(i -> {
                     if (i % 2 == 0) {
-                        scheduledExecutorService.scheduleAtFixedRate(() -> copyOnWriteArraySet.add(i), 0, 100, TimeUnit.MILLISECONDS);
+                        scheduledExecutorService.scheduleAtFixedRate(() -> copyOnWriteArraySet.add(i), 0, 50, TimeUnit.MILLISECONDS);
                     } else {
-                        scheduledExecutorService.scheduleWithFixedDelay(() -> copyOnWriteArraySet.add(i), 0, 100, TimeUnit.MILLISECONDS);
+                        scheduledExecutorService.scheduleWithFixedDelay(() -> copyOnWriteArraySet.add(i), 0, 50, TimeUnit.MILLISECONDS);
                     }
                 });
         TimeUnit.SECONDS.sleep(2);
-        System.out.println(copyOnWriteArraySet);
+        System.out.println("1: " + copyOnWriteArraySet);
     }
 
     void m2() throws InterruptedException {
-        scheduledExecutorService.schedule(()->copyOnWriteArraySet.forEach(i->copyOnWriteArraySet.remove(i)),  100, TimeUnit.MILLISECONDS);
+        scheduledExecutorService.schedule(()->copyOnWriteArraySet.forEach(i->copyOnWriteArraySet.remove(i)),  1000, TimeUnit.MILLISECONDS);
         TimeUnit.SECONDS.sleep(2);
         System.out.println(scheduledExecutorService);
-        System.out.println(copyOnWriteArraySet);
+        System.out.println("2: " + copyOnWriteArraySet);
         scheduledExecutorService.shutdownNow();
     }
     public static void main(String[] args) throws InterruptedException {
