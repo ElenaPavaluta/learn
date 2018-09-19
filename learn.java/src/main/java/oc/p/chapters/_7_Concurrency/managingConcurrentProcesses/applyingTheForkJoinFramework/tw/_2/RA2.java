c7e3d3c3a18481d4c1bea3e17dff83f09c8f0ecf
Ciprian Tanase <ciprian.dorin.tanase@ibm.com>
2018-09-19 15:41:37 +0300

    commit

---
+++ learn.java/src/main/java/oc/p/chapters/_7_Concurrency/managingConcurrentProcesses/applyingTheForkJoinFramework/tw/_2/RA2.java
@@ -0,0 +1,10 @@
+package oc.p.chapters._7_Concurrency.managingConcurrentProcesses.applyingTheForkJoinFramework.tw._2;
+
+
+import java.util.concurrent.ForkJoinTask;
+
+class RA2 {
+
+//    ForkJoinTask<String>
+}
+
